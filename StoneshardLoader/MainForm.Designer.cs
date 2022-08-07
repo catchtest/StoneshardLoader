26bc93e8bc74e9e7fdeb28bfe5820f61515aee7e
catchtest <catchtest@gmail.com>
2022-08-08 00:21:00 +0800

    First commit

---
+++ StoneshardLoader/MainForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace StoneshardLoader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblMessage = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.cboSave = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lstSaveFolder = new System.Windows.Forms.ListBox();
+            this.picPreview = new System.Windows.Forms.PictureBox();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(228, 123);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(211, 29);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "&Copy exitsave";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLoad.Location = new System.Drawing.Point(227, 160);
+            this.btnLoad.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(211, 29);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "&Load last exitsave";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblMessage});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 383);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 16, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(452, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 17);
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpenFolder.Location = new System.Drawing.Point(228, 197);
+            this.btnOpenFolder.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(211, 29);
+            this.btnOpenFolder.TabIndex = 3;
+            this.btnOpenFolder.Text = "&Open folder";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolder_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.Location = new System.Drawing.Point(228, 234);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(211, 29);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "&Delete all but last exitsave";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // cboSave
+            // 
+            this.cboSave.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboSave.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboSave.FormattingEnabled = true;
+            this.cboSave.Location = new System.Drawing.Point(12, 123);
+            this.cboSave.Margin = new System.Windows.Forms.Padding(4);
+            this.cboSave.Name = "cboSave";
+            this.cboSave.Size = new System.Drawing.Size(208, 24);
+            this.cboSave.TabIndex = 0;
+            this.cboSave.SelectedIndexChanged += new System.EventHandler(this.cboSave_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(452, 116);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lstSaveFolder
+            // 
+            this.lstSaveFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstSaveFolder.FormattingEnabled = true;
+            this.lstSaveFolder.IntegralHeight = false;
+            this.lstSaveFolder.ItemHeight = 16;
+            this.lstSaveFolder.Location = new System.Drawing.Point(12, 154);
+            this.lstSaveFolder.Name = "lstSaveFolder";
+            this.lstSaveFolder.Size = new System.Drawing.Size(208, 216);
+            this.lstSaveFolder.TabIndex = 7;
+            this.lstSaveFolder.SelectedIndexChanged += new System.EventHandler(this.lstSaveFolder_SelectedIndexChanged);
+            this.lstSaveFolder.DoubleClick += new System.EventHandler(this.lstSaveFolder_DoubleClick);
+            // 
+            // picPreview
+            // 
+            this.picPreview.Location = new System.Drawing.Point(228, 270);
+            this.picPreview.Name = "picPreview";
+            this.picPreview.Size = new System.Drawing.Size(210, 100);
+            this.picPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picPreview.TabIndex = 8;
+            this.picPreview.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 405);
+            this.Controls.Add(this.picPreview);
+            this.Controls.Add(this.lstSaveFolder);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cboSave);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Controls.Add(this.btnSave);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Stoneshard Loader";
+            this.Activated += new System.EventHandler(this.MainForm_Activated);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblMessage;
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ComboBox cboSave;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox lstSaveFolder;
+        private System.Windows.Forms.PictureBox picPreview;
+    }
+}
+
---
+++ StoneshardLoader/MainForm.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace StoneshardLoader
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private string BasePath;
+        private string[] CharacterFolders;
+        private string CharacterPath;
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var sourcePath = Path.Combine(CharacterPath, "exitsave_1");
+            if (!Directory.Exists(sourcePath))
+            {
+                MessageBox.Show("Can't find save folder.", "Error");
+                return;
+            }
+
+            var targetPath = Path.Combine(CharacterPath, $"exitsave_1_{DateTime.Now:yyyyMMdd_HHmmss}");
+            if (Directory.Exists(targetPath))
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+            Directory.CreateDirectory(targetPath);
+
+            CopyDirectory(sourcePath, targetPath);
+            lblMessage.Text = "Save file copied.";
+            LoadSaveFolders();
+        }
+
+        static void CopyDirectory(string sourcePath, string targetPath)
+        {
+            foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+            {
+                Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
+            }
+
+            foreach (string newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+            {
+                File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            var targetPath = Path.Combine(CharacterPath, "exitsave_1");
+            if (Directory.Exists(targetPath))
+            {
+                MessageBox.Show("Save folder already exists", "Error");
+                return;
+            }
+
+            var folders = Directory.GetDirectories(CharacterPath, "exitsave_1_*");
+            if (!folders.Any())
+            {
+                MessageBox.Show("Can't find save path.", "Error");
+                return;
+            }
+
+            // 取得最後一個
+            var sourcePath = Path.Combine(CharacterPath, folders.OrderByDescending(x => x).FirstOrDefault());
+            Directory.CreateDirectory(targetPath);
+            CopyDirectory(sourcePath, targetPath);
+
+            lblMessage.Text = "Save file loaded.";
+            LoadSaveFolders();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            var localAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var basePath = Path.Combine(localAppDataPath, "StoneShard", "characters_v1");
+
+            if (!Directory.Exists(basePath))
+            {
+                MessageBox.Show($"Can't find {basePath}");
+                Close();
+                return;
+            }
+            BasePath = basePath;
+
+            LoadCharacterFolders();
+            AutoSelectLastFolder();
+        }
+
+        private void AutoSelectLastFolder()
+        {
+            var lastFolder = Properties.Settings.Default["LastSelectFolder"] as string;
+            if (!string.IsNullOrEmpty(lastFolder) && CharacterFolders.Contains(lastFolder))
+            {
+                cboSave.SelectedItem = lastFolder;
+                return;
+            }
+
+            Properties.Settings.Default["LastSelectFolder"] = null;
+            Properties.Settings.Default.Save();
+        }
+
+        private void LoadCharacterFolders()
+        {
+            var saveFolders = Directory.GetDirectories(BasePath, "character_*");
+            CharacterFolders = saveFolders.Select(x => Path.GetFileName(x)).ToArray();
+            cboSave.Items.AddRange(CharacterFolders);
+        }
+
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            Process.Start(CharacterPath);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            var folders = Directory.GetDirectories(CharacterPath, "exitsave_1_*");
+            if (folders.Count() <= 1)
+            {
+                MessageBox.Show("No other folder.", "Info");
+                return;
+            }
+
+            var deleteFolders = folders.OrderByDescending(x => x).Skip(1);
+            foreach (var folder in deleteFolders)
+            {
+                Directory.Delete(folder, true);
+            }
+            lblMessage.Text = "Delete successfully.";
+            LoadSaveFolders();
+        }
+
+        private void cboSave_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var folderName = cboSave.SelectedItem.ToString();
+            var basePath = Path.Combine(BasePath, folderName);
+
+            if (Directory.Exists(basePath))
+            {
+                CharacterPath = basePath;
+                LoadSaveFolders();
+
+                Properties.Settings.Default["LastSelectFolder"] = folderName;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                MessageBox.Show($"Folder {basePath} not found.");
+                cboSave.SelectedIndex = -1;
+                LoadCharacterFolders();
+            }
+        }
+
+        private void LoadSaveFolders()
+        {
+            var folders = Directory.GetDirectories(CharacterPath);
+            folders = folders.Select(x => Path.GetFileName(x)).ToArray();
+
+            lstSaveFolder.Items.Clear();
+            lstSaveFolder.Items.AddRange(folders);
+
+            LoadThumbnail();
+        }
+
+        private void LoadThumbnail()
+        {
+            var selectFolder = lstSaveFolder.SelectedItem?.ToString();
+            if (string.IsNullOrEmpty(selectFolder))
+            {
+                selectFolder = lstSaveFolder.Items.Cast<string>().Where(x => x.StartsWith("exitsave_1_")).OrderByDescending(x => x).FirstOrDefault();
+            }
+            if (string.IsNullOrEmpty(selectFolder))
+            {
+                picPreview.ImageLocation = null;
+            }
+            var previewPath = Path.Combine(CharacterPath, selectFolder, "preview.png");
+            if (File.Exists(previewPath))
+            {
+                picPreview.ImageLocation = previewPath;
+            }
+            else
+            {
+                picPreview.ImageLocation = null;
+            }
+        }
+
+        private void lstSaveFolder_DoubleClick(object sender, EventArgs e)
+        {
+            var folder = lstSaveFolder.SelectedItem?.ToString();
+            if (!string.IsNullOrEmpty(folder))
+            {
+                Process.Start(Path.Combine(CharacterPath, folder));
+            }
+        }
+
+        private void lstSaveFolder_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadThumbnail();
+        }
+
+        private void MainForm_Activated(object sender, EventArgs e)
+        {
+            LoadSaveFolders();
+        }
+    }
+}
